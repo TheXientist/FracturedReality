8646be4ea15dc33585ee00dc1bc0df82596ed0ef
Simon Hetzer (desktop) <simon.hetzer@icloud.com>
2023-06-15 11:07:24 +0200

    Deleted Shader test subproject

--- Shader test/Assets/BossScripts/BossBullets/AbstractBullet.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AbstractBullet : MonoBehaviour
-{
-    [SerializeField]
-    protected int m_DamageValue;
-}
