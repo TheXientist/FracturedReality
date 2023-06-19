e639c3d7c2c20a0275593fb0031c86d23f2adab5
Christoph Schulze <christophschulze93@gmail.com>
2023-06-19 10:52:10 +0200

    added blue particle effect for player projectile

--- Assets/Scripts/PlayerGameplayScripts/PlayerProjectiles/PlayerDefaultProjectile.cs
+++ Assets/Scripts/PlayerGameplayScripts/PlayerProjectiles/PlayerDefaultProjectile.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 
 public class PlayerDefaultProjectile : AbstractBullet
 {
+    public ParticleSystem particleImpactEffect;
+    private MeshRenderer m_ProjectileMesh;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_ProjectileMesh = GetComponent<MeshRenderer>();
     }
 
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Boss") || other.CompareTag("Obstacle"))
         {
             other.GetComponent<IDamageable>().TakeDamage(base.m_DamageValue);
-            Destroy(gameObject);
+            StartCoroutine(PlayCollisionEffect());
         }
+    }
+
+    private IEnumerator PlayCollisionEffect()
+    {
+        transform.SetParent(null, true);
+        m_ProjectileMesh.enabled = false;
+        particleImpactEffect.Play();
+        yield return new WaitForSeconds(1);
+        DestroySelf();
     }
 
 }
