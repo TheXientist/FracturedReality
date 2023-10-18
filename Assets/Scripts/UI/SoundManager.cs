5fe801593b3a1b91c2974f712f6d88d16fcfb11c
Simon Hetzer (desktop) <simon.hetzer@icloud.com>
2023-10-18 16:35:24 +0200

    Added Clamping and % Display to Sound UI

--- Assets/Scripts/UI/SoundManager.cs
+++ Assets/Scripts/UI/SoundManager.cs
@@ -9,7 +9,7 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
     private AudioMixer m_audioMixer;
 
-    float m_stepSize = 1f;
+    float m_stepSize = 2f;
 
     float m_masterVolume = 0f;
     float m_musicVolume = 0f;
@@ -25,60 +25,52 @@ public class SoundManager : MonoBehaviour
         m_audioMixer.GetFloat("MusicParam", out m_musicVolume);
         m_audioMixer.GetFloat("SFXParam", out m_SFXVolume);
         m_audioMixer.GetFloat("MasterParam", out m_SFXVolume);
+        
+        PrintVolume(m_master, m_masterVolume);
+        PrintVolume(m_music, m_musicVolume);
+        PrintVolume(m_SFX, m_SFXVolume);
     }
 
+    private void PrintVolume(TextMeshProUGUI uiElement, float volume)
+    {
+        int perc = (int)(((volume + 40f) / 60f) * 100);
+        uiElement.text = perc.ToString() + '%';
+    }
+    
     public void ChangeMusicVolume(bool increase)
     {
         if (increase)
-        {
             m_musicVolume += m_stepSize;
-
-            m_audioMixer.SetFloat("MusicParam", m_musicVolume);
-        }
         else
-        {
             m_musicVolume -= m_stepSize;
-            m_audioMixer.SetFloat("MusicParam", m_musicVolume);
-        }
 
-        m_music.text = m_musicVolume.ToString();
-
+        m_musicVolume = Mathf.Clamp(m_musicVolume, -40f, 20f);
+        m_audioMixer.SetFloat("MusicParam", m_musicVolume);
+        PrintVolume(m_music, m_musicVolume);
     }
 
     public void ChangeSFXVolume(bool increase)
     {
         if (increase)
-        {
             m_SFXVolume += m_stepSize;
-
-            m_audioMixer.SetFloat("SFXParam", m_SFXVolume);
-        }
         else
-        {
             m_SFXVolume -= m_stepSize;
-            m_audioMixer.SetFloat("SFXParam", m_SFXVolume);
-        }
-
-        m_SFX.text = m_SFXVolume.ToString();
-
+        
+        m_SFXVolume = Mathf.Clamp(m_SFXVolume, -40f, 20f);
+        m_audioMixer.SetFloat("SFXParam", m_SFXVolume);
+        PrintVolume(m_SFX, m_SFXVolume);
     }
 
     public void ChangeMasterVolume(bool increase)
     {
         if (increase)
-        {
             m_masterVolume += m_stepSize;
-
-            m_audioMixer.SetFloat("MasterParam", m_masterVolume);
-        }
         else
-        {
             m_masterVolume -= m_stepSize;
-            m_audioMixer.SetFloat("MasterParam", m_masterVolume);
-        }
 
-        m_master.text = m_masterVolume.ToString();
-
+        m_masterVolume = Mathf.Clamp(m_masterVolume, -40f, 20f);
+        m_audioMixer.SetFloat("MasterParam", m_masterVolume);
+        PrintVolume(m_master, m_masterVolume);
     }
 
 }
